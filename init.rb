f5f4b0a079b66c67875c48a9303254b12b1cab15
tnaka176 <67179870+tnaka176@users.noreply.github.com>
2023-01-16 16:13:16 +0900

    fix require_method

--- init.rb
+++ init.rb
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+require 'redmine'
+require_relative 'lib/good_job_hook_view_listeners'
 
 Redmine::Plugin.register :redmine_good_job do
   name 'Redmine Good Job plugin'
